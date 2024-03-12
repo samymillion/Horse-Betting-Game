0ff4a491e6d95065e73351e27085afec32d8ffa5
Anna Ma <ama92@uwo.ca>
2024-03-12 19:45:57 -0400

    commenting for bet class

--- bet.cpp
+++ bet.cpp
@@ -1,43 +1,70 @@
 #include "bet.h"
 
+//! A constructor.
+/*!
+    A more elaborate description of the constructor.
+*/
 Bet::Bet(double initialMoneyPool, QObject *parent) : QObject(parent), m_betAmount(0.0), m_moneyPool(initialMoneyPool)
 {
 
 }
 
+//! A setter function for bet amount that takes 1 argument and sets the attribute m_betAmount.
+/*!
+    \param amount an double argument that represents the amount bet by the user.
+*/
 void Bet::setBetAmount(double amount)
 {
     m_betAmount = amount;
 }
 
+//! A setter function for money pool that takes 1 argument and sets the attribute m_moneyPool.
+/*!
+    \param amount an double argument that represents the current money pool of the user.
+*/
 void Bet::setMoneyPool(double amount)
 {
     m_moneyPool = amount;
 }
 
+//! A getter function for bet amount that returns the attribute m_betAmount.
+/*!
+    \return the amount bet by the user.
+*/
 double Bet::getBetAmount() const
 {
     return m_betAmount;
 }
 
+//! A getter function for bet amount that returns the attribute m_moneyPool.
+/*!
+    \return the user's current money pool.
+*/
 double Bet::getMoneyPool() const
 {
     return m_moneyPool;
 }
 
+//! A function that returns true and deducts the user's bet amount from money pool. If the bet is greater than the money pool, return false.
+/*!
+    \return true if the bet is within the limits of the money pool, false otherwise.
+*/
 bool Bet::placeBet()
 {
-    if (m_betAmount > m_moneyPool) {
-        // Bet exceeds money pool
+    if (m_betAmount > m_moneyPool) { // if bet exceeds money pool return false
         return false;
     } else {
-        // Deduct bet amount from money pool
-        m_moneyPool -= m_betAmount;
-        emit moneyPoolChanged(m_moneyPool);
+        m_moneyPool -= m_betAmount; // if bet is valid, deduct bet amount from money pool
+        emit moneyPoolChanged(m_moneyPool); // emit signal that money pool changed
         return true;
     }
 }
 
+//! A function that takes the bet amount set by the user and returns two times the initial bet to represent the payout (payout is 200% of bet amount).
+/*!
+    \param amount the bet amount that the user set this round.
+    \return double the initial bet amount, which represents the payout.
+*/
 double Bet::calculatePayout(double amount){
     return amount+amount;
 }
--- bet.h
+++ bet.h
@@ -1,3 +1,5 @@
+//!  A bet class that contains a bet object to store information on bet amount, money pool, and calculates payout. 
+
 #ifndef BET_H
 #define BET_H
 
@@ -7,19 +9,58 @@ class Bet : public QObject
 {
     Q_OBJECT
 public:
+    //! A constructor.
+    /*!
+      A more elaborate description of the constructor.
+    */
     explicit Bet(double initialMoneyPool, QObject *parent = nullptr);
+    
+    //! A setter function for bet amount that takes 1 argument and sets the attribute m_betAmount.
+    /*!
+      \param amount an double argument that represents the amount bet by the user.
+    */
     void setBetAmount(double amount);
+
+    //! A setter function for money pool that takes 1 argument and sets the attribute m_moneyPool.
+    /*!
+      \param amount an double argument that represents the current money pool of the user.
+    */
     void setMoneyPool(double amount);
+
+    //! A getter function for bet amount that returns the attribute m_betAmount.
+    /*!
+      \return the amount bet by the user.
+    */
     double getBetAmount() const;
+
+    //! A getter function for bet amount that returns the attribute m_moneyPool.
+    /*!
+      \return the user's current money pool.
+    */
     double getMoneyPool() const;
+
+    //! A function that returns true and deducts the user's bet amount from money pool. If the bet is greater than the money pool, return false.
+    /*!
+      \return true if the bet is within the limits of the money pool, false otherwise.
+    */
     bool placeBet();
+
+    //! A function that takes the bet amount set by the user and returns two times the initial bet to represent the payout (payout is 200% of bet amount).
+    /*!
+      \param amount the bet amount that the user set this round.
+      \return double the initial bet amount, which represents the payout.
+    */
     double calculatePayout(double amount);
+
 signals:
     void moneyPoolChanged(double newAmount);
 public slots:
 
 private:
+    //! private variable that stores current bet amount
     double m_betAmount;
+
+    //! private variable that stores the current money pool
     double m_moneyPool;
 };
 
